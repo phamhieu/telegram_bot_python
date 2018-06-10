8e53c13a42982844e0251d3ab68afbefe736762e
Pham Hieu <phamhieu1998@gmail.com>
2018-06-10 09:27:13 +0800

    Create settings page and support login/logout and 403

--- app/handlers/base.py
+++ app/handlers/base.py
@@ -2,6 +2,8 @@ import webapp2
 import jinja2
 import os
 
+from google.appengine.api import users
+
 template_dir = os.path.join(os.path.dirname(__file__), '../templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                extensions=['jinja2.ext.autoescape'],
@@ -31,3 +33,47 @@ class AboutPage(Handler):
             'title': 'About',
         }
         self.render_template('about.html', **kwargs)
+
+
+class SettingsPage(Handler):
+    def __admin_required(func):
+        def func_wrapper(self, *args, **kwargs):
+            user = users.get_current_user()
+            if user:
+                email = user.email()
+                admins = [
+                    "admin1@gmail.com",
+                    "admin2@gmail.com"
+                ]
+                if email in admins:
+                    return func(self, *args, **kwargs)
+                else:
+                    return webapp2.redirect_to("403")
+            else:
+                return webapp2.redirect_to("login")
+        return func_wrapper
+
+    @__admin_required
+    def get(self):
+        kwargs = {
+            'title': 'Settings',
+        }
+        self.render_template('settings.html', **kwargs)
+
+
+class LoginHandler(Handler):
+    def get(self):
+        self.redirect(users.create_login_url('/'))
+
+
+class LogoutHandler(Handler):
+    def get(self):
+        self.redirect(users.create_logout_url('/'))
+
+
+class Page403(Handler):
+    def get(self):
+        kwargs = {
+            'title': '403 Forbidden',
+        }
+        self.render_template('403.html', **kwargs)
--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import webapp2
 from app.handlers import base, bot
 
 app = webapp2.WSGIApplication([
+    webapp2.Route('/login', base.LoginHandler, name='login'),
+    webapp2.Route('/logout', base.LogoutHandler, name='logout'),
+    webapp2.Route('/403', base.Page403, name='403'),
     ('/', base.MainPage),
     ('/about', base.AboutPage),
+    ('/settings', base.SettingsPage),
     ('/set_webhook', bot.SetWebhookHandler),
     ('/get_webhook', bot.GetWebhookHandler),
     ('/webhook', bot.WebhookHandler),
