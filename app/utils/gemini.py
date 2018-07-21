e552e387bbd788710904c0bfe00d6ed181c674a2
Pham Hieu <phamhieu1998@gmail.com>
2018-07-21 09:30:27 +0800

    update gemini util to support getBookingPrice

--- app/utils/gemini.py
+++ app/utils/gemini.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from decimal import Decimal
 
 from google.appengine.api import urlfetch
 
@@ -17,8 +18,36 @@ def get_btc_price():
         else:
             logging.exception(
                 'get_btc_price failed {}'.format(result.status_code)
-            )
+                )
             return None
     except:
         logging.exception('get_btc_price exception fetching url')
         return None
+
+
+def get_btc_booking():
+    url = __URL_PRODUCTION + "/book/btcusd"
+    try:
+        result = urlfetch.fetch(url)
+        if result.status_code == 200:
+            jsonstr = result.content
+            jsonobj = json.loads(jsonstr)
+            bids = jsonobj['bids']
+            max_bid = 0
+            for bid in bids:
+                price = Decimal(bid['price'])
+                max_bid = price if price > max_bid else max_bid
+            asks = jsonobj['asks']
+            min_ask = 999999999
+            for ask in asks:
+                price = Decimal(ask['price'])
+                min_ask = price if price < min_ask else min_ask
+            return max_bid, min_ask
+        else:
+            logging.exception(
+                'get_btc_booking failed {}'.format(result.status_code)
+                )
+            return None, None
+    except:
+        logging.exception('get_btc_booking exception fetching url')
+        return None, None
